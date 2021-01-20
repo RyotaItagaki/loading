3cfea6209138425094920e938f44397e32e30c16
Ryota Itagaki <ribitamana@gmail.com>
2021-01-20 16:25:24 +0900

    first commit

---
+++ js/loading.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+  const loading = document.getElementById('loading');
+  loading.classList.add('loaded');
+  // document.getElementById('loading').classList.add('loaded');
+}
